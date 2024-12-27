9b326e36e15dcb5d334793d9d116125c4fd59701
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2024-12-27 18:43:59 +0530

    s

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = ['react-native-reanimated/plugin'];
+  const plugins = [
+    'react-native-reanimated/plugin',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+  ];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
